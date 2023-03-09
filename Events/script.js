44d1f05d0947feaaf4dbbdecf4c916c97e05ed64
Aanya Rawat <77845460+Aanya74282@users.noreply.github.com>
2023-03-09 10:48:54 +0530

    Update script.js

--- Events/script.js
+++ Events/script.js
@@ -4,7 +4,7 @@ const eventsNames = [
   'Aaghaz',
   'Rap Battle',
   'Group Singing-Antra',
-  'Solo Singing-Anhad',
+  'Solo Singing-Alaap',
   'Lilac Dreams',
   'Jashn-e-Alfaaz',
   'Urban Thump',
@@ -18,7 +18,7 @@ const texts = [
   "“The theatre was created to tell people the truth about life and the social situation.“<br>-Stella Adler.<br><br>The nukkad-natak competition, Aaghaz, offers a stage for all performers to captivate the audience with enriching performances. Here is your chance to make your voice heard loud and clear.Stand up, show up and make the stage blow up!",
   "“The life you live is more important than the words you speak.“<br>– Mac Miller<br><br>Rap is an ideal medium for zealous lyrical sagacity, with real-true verses and soulful rhythms that can captivate anyone. Taarangana’23 presents you with the Rap Battle, a stage for you to exhibit your euphonious talent that wins everyone's hearts.",
   "“Music, once admitted to the soul, becomes a sort of spirit and never dies.“<br>-Edward Bulwer Lytton.<br><br>Antra is a group singing competition for all the euphonious groups that know, without a doubt, how to give their lives to music. We encourage every music enthusiast to come forth and subdue the crowd as they hit the notes; diverse talent calls for a demonstration. Join us and set the tone for the evening.",
-  "“Only thing better than singing is more singing!“<br><br>Anhad, the solo singing competition, is for all the melodious souls with soothing tunes. It is a solo singing contest in which contestants compete against each other to test their vocal ability, performance, stage presence, and originality. Here, passion for music is the only prerequisite, so hop on to captivate the crowd with your charming voice and win many hearts!",
+  "“Only thing better than singing is more singing!“<br><br>Alaap, the solo singing competition, is for all the melodious souls with soothing tunes. It is a solo singing contest in which contestants compete against each other to test their vocal ability, performance, stage presence, and originality. Here, passion for music is the only prerequisite, so hop on to captivate the crowd with your charming voice and win many hearts!",
   "“Style is a way to say who you are without having to speak.“<br> – Rachel Zoe <br><br>Lilac Dreams is a fashion show competition designed for fashionistas to express their emotions through outfits and artwork. If you can tell your fashion tale without talking, the runaway is all yours. Bring out the diva in you and conquer the ramp with your rapturous grace!!",
   "“True poetry is when emotion finds thought, and the thought finds words.“<br><br>A poet is an artist who knows the art of giving life to words. They can make the listeners travel into the world of imagination with them. Taarangana’23 presents Jashn-e-Alfaaz, the solo poetry contest, a stage for budding orators possessing the art of poetry to exhibit their forte. Join us to behold the art of poetry in its purest form. ",
   "“Dance is a pulse, a heartbeat, breathing. It’s the rhythm of your life. It’s the expression, in time and movement, of happiness, joy, sadness, and envy.“<br><br>Urban Thump is a group dance competition for those teams who enjoy swaying to the beats and falling in love with the stage every time. Showcase an escapade, undulate your body, and let the dancer in you reach out. Join us if your team has got those elegant yet fierce wacks and punks."
